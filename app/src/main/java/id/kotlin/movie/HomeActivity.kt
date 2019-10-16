f078dc33d44d95009be9c2e7fd692ad50fe5e2fa
Budi Oktaviyan <budioktaviyans@gmail.com>
2019-10-17 00:37:24 +0700

    Initial databinding

---
+++ app/src/main/java/id/kotlin/movie/HomeActivity.kt
@@ -0,0 +1,12 @@
+package id.kotlin.movie
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_home)
+  }
+}
